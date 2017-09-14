14cb44cad3a208ffa3f97633b6a91befee10e5c9
JunePaloma <kborkoski@gmail.com>
2017-09-14 10:24:11 -0600

    Add relationships in machine and snack models

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,6 +1,7 @@
 class Machine < ApplicationRecord
   belongs_to :owner
-  has_many :snacks
+  has_many :snack_machines
+  has_many :snacks, through: :snack_machines
 
   def average_snack_price
     snacks.average(:price)
--- app/models/snack.rb
+++ app/models/snack.rb
@@ -1,3 +1,4 @@
 class Snack < ApplicationRecord
-  belongs_to :machine
+  has_many :snack_machines
+  has_many :machines, through: :snack_machines
 end
