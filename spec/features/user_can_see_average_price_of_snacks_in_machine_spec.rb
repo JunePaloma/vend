f584ddec04944880c8225870d36f7a32387056d5
JunePaloma <kborkoski@gmail.com>
2017-09-14 09:53:53 -0600

    Add passing test and functionality for second user story, visitor can see average price of snacks

--- spec/features/user_can_see_average_price_of_snacks_in_machine_spec.rb
+++ spec/features/user_can_see_average_price_of_snacks_in_machine_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 describe "As a user" do
 describe "When I visit a specific vending machine page" do
-  it "I see the name of all of the snacks associated with that vending machine along with their price " do
-
+  it "I see the average price for the snacks in that machine " do
     owner = Owner.create(name: "Sam's Snacks")
     dons_machine  = owner.machines.create(location: "Don's Mixed Drinks")
 
     chips = dons_machine.snacks.create(name: "chips", price:2.50)
     candy = dons_machine.snacks.create(name: "candy", price:2.50)
+    hot_chips = dons_machine.snacks.create(name: "hot chips", price:3.0)
+    banana = dons_machine.snacks.create(name: "hot chips", price:3.0)
 
     visit machine_path(dons_machine)
 
-    expect(page).to have_content("Snack:chips")
+    expect(page).to have_content("Average price: $2.75")
 
-    expect(page).to have_content("Price: $2.5")
-    expect(page).to have_content("Snack: candy")
+
     end
   end
 end
